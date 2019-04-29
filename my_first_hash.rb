040741323a44e0201bc527d2482f5e7b8c877ecc
Renee Nordholm <reneenordholm@gmail.com>
2019-04-29 01:43:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   cat = {
-    cat_one => "Hansel",
-    cat_two => "Frenchie"
+    "First Cat" => "Hansel",
+    "Second Cat" => "Frenchie"
   }
   
 end
